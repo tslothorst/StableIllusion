d95d3a8bdfb97b65ea4f600cfb9d6b5066f88627
tslothorst <tslothorst@gmail.com>
2023-06-09 00:26:32 +0200

    Add folders for models with placeholders

--- StableIllusion/StableIllusion/MainWindow.xaml.cs
+++ StableIllusion/StableIllusion/MainWindow.xaml.cs
@@ -1,24 +1,10 @@
 ﻿using StableDiffusion.ML.OnnxRuntime;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using StableIllusion.Extensions;
-using System.Drawing;
-using System.IO;
-using System.Drawing.Imaging;
 using System.Globalization;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 
 namespace StableIllusion
@@ -35,8 +21,6 @@ namespace StableIllusion
             this.settings = settings.Value;
             InitializeComponent();
         }
-
-        private Task ImageTask = null;
 
         private async void btnGenerate_Click(object sender, RoutedEventArgs e)
         {
